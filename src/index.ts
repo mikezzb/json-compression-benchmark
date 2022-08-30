2ef33738fa84fb3e0a3594830e9e416f6efc69dd
mikezzb <mikezhoudev@gmail.com>
2022-08-30 16:51:03 +0800

    Update output format

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,27 @@ type StatResult = {
 
 type Stat = Record<JsonMinifyMethod, StatResult>;
 
+type CompressedMap = Record<JsonMinifyMethod, string>;
+
+type BenchmarkResult = {
+  stat: Stat;
+  compressed: CompressedMap;
+};
+
 const methods: JsonMinifyMethod[] = ['jsonpack', 'lzw'];
 
-const _compress = withTimer((method: JsonMinifyMethod) => {
-  switch (method) {
-    case 'jsonpack':
-      break;
-    case 'lzw':
-      break;
+const _compress = withTimer<string>(
+  (jsonStr: string, method: JsonMinifyMethod) => {
+    switch (method) {
+      case 'jsonpack':
+        break;
+      case 'lzw':
+        break;
+    }
   }
-});
+);
 
-const _decompress = withTimer((method: JsonMinifyMethod) => {
+const _decompress = withTimer((jsonStr: string, method: JsonMinifyMethod) => {
   switch (method) {
     case 'jsonpack':
       break;
@@ -32,20 +41,42 @@ const _decompress = withTimer((method: JsonMinifyMethod) => {
 
 export default class JsonStat {
   private stat: Stat = {} as any;
-  benchmark(obj: Record<string, any> | string) {
-    obj = typeof obj === 'string' ? obj : JSON.stringify(obj);
-    console.log(obj);
+  private compressed: CompressedMap = {} as any;
+  private jsonStr?: string;
+  benchmark(
+    obj: Record<string, any> | string,
+    verbose = false
+  ): BenchmarkResult {
+    this.jsonStr = typeof obj === 'string' ? obj : JSON.stringify(obj);
+    console.log(this.jsonStr);
     methods.forEach(method => {
-      const cStat = this.compress(method);
+      const [compressed, cStat] = this.compress(method);
       const pStat = this.decompress(method);
       this.stat[method] = { ...cStat, ...pStat } as any;
+      this.compressed[method] = compressed as any;
     });
-    return this.stat;
+    const result = {
+      stat: this.stat,
+      compressed: this.compressed,
+    };
+    if (verbose) console.log(result);
+    return result;
   }
-  compress(method: JsonMinifyMethod): Partial<StatResult> {
-    return {};
+  compress(method: JsonMinifyMethod): [string, Partial<StatResult>] {
+    const { result: jsonMinStr, time } = _compress(this.jsonStr, method);
+    console.log(`Compress: ${time}ms - ${method}`);
+    return [
+      jsonMinStr,
+      {
+        compressTime: time,
+      },
+    ];
   }
   decompress(method: JsonMinifyMethod): Partial<StatResult> {
-    return {};
+    const { time } = _decompress(this.jsonStr, method);
+    console.log(`Decompress: ${time}ms - ${method}`);
+    return {
+      decompressTime: time,
+    };
   }
 }
